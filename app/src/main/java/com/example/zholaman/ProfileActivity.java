b9b50f2687ed795ad3cc39b9c262827af143bad7
Aidana <foreducationmoonnis@gmail.com>
2022-05-25 16:12:52 +0600

    Changing history activity parameters

--- app/src/main/java/com/example/zholaman/ProfileActivity.java
+++ app/src/main/java/com/example/zholaman/ProfileActivity.java
@@ -1,21 +1,26 @@
 package com.example.zholaman;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView historybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        historybutton = (ImageView) findViewById(R.id.iconmenu);
         bottomNavigationView.setSelectedItemId(R.id.profile);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
@@ -41,5 +46,12 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+            historybutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(),HistoryActivity.class));
+                }
+            });
     }
 }
